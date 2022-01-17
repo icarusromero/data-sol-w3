dc178d30a43dcce2a6e173841347d2bf5785a1a0
Icarus Romero <cloudkingdom07@yahoo.com>
2022-01-16 23:55:20 -0600

    striped year off movie title, changed date to datetime

--- ratings-seed/ratings-seed/seed.py
+++ ratings-seed/ratings-seed/seed.py
@@ -2,8 +2,9 @@
 
 from sqlalchemy import func
 from model import User
-# from model import Rating
-# from model import Movie
+from model import Rating 
+from model import Movie
+from datetime import datetime
 
 from model import connect_to_db, db
 from server import app
@@ -37,9 +38,42 @@ def load_users():
 def load_movies():
     """Load movies from u.item into database."""
 
+    Movie.query.delete()
+
+    for row in open('seed_data/u.item'):
+        row = row.rstrip
+        movie_id, title, released_at, imbd_url = row.split('|')
+        title = title[:-7]
+
+        format = "%d-%b-%Y"
+        released_at = datetime.strptime(released_at, format)
+
+        movie = Movie(movie_id=movie_id,
+        title=title,
+        released_at=released_at,
+        imbd_url=imbd_url)
+
+        db.session.add(movie)
+
+    db.session.commit()
+
 
 def load_ratings():
     """Load ratings from u.data into database."""
+
+    Rating.query.delete()
+
+    for row in open('seed_data/u.data'):
+        row = row.rstrip()
+        movie_id, user_id, score = row.split(' ')
+
+        rating = Rating(movie_id=movie_id,
+        user_id=user_id,
+        score=score)
+
+        db.session.add(rating)
+
+    db.session.commit()
 
 
 def set_val_user_id():
